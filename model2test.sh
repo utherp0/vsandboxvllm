e24de5a5d71ff7eb2646680f8c495a2932105854
utherp0 <ian.lawson@redhat.com>
2025-11-24 12:35:08 +0000

    Corrected model2

--- model2test.sh
+++ model2test.sh
@@ -1,1 +1,1 @@
-curl -kL -H "Authorization: Bearer "$(oc whoami -t) https://isvc-granite-31-8b-fp8-predictor.sandbox-shared-models.svc.cluster.local:8443/v1/models
+curl -kL -H "Authorization: Bearer "$(oc whoami -t) https://isvc-nemotron-nano-9b-v2-fp8-predictor.sandbox-shared-models.svc.cluster.local:8443/v1/models
